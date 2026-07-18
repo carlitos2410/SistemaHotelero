e856a1dfd8a24f6843b3d2a96a594a99ec726e07
juancordova-b <cbarrantesjuanc@uss.edu.pe>
2026-07-17 20:22:00 -0500

    Registra modelos de habitaciones en el admin de Django con list_display y filtros

--- habitaciones/admin.py
+++ habitaciones/admin.py
@@ -1,1 +1,33 @@
-"""Registro administrativo de habitaciones pendiente de implementación."""
+from django.contrib import admin
+
+from .models import Habitacion, HabitacionEstadoHistorial, ObservacionMantenimiento, TipoHabitacion
+
+
+@admin.register(TipoHabitacion)
+class TipoHabitacionAdmin(admin.ModelAdmin):
+    list_display = ['nombre', 'capacidad', 'precio_base']
+    search_fields = ['nombre']
+
+
+@admin.register(Habitacion)
+class HabitacionAdmin(admin.ModelAdmin):
+    list_display = ['numero', 'hotel', 'tipo', 'piso', 'estado']
+    list_filter = ['estado', 'hotel', 'tipo', 'piso']
+    search_fields = ['numero']
+    list_select_related = ['hotel', 'tipo']
+
+
+@admin.register(HabitacionEstadoHistorial)
+class HabitacionEstadoHistorialAdmin(admin.ModelAdmin):
+    list_display = ['habitacion', 'estado_anterior', 'estado_nuevo', 'cambiado_por', 'cambiado_en']
+    list_filter = ['estado_nuevo', 'cambiado_en']
+    list_select_related = ['habitacion', 'cambiado_por']
+    readonly_fields = ['cambiado_en']
+
+
+@admin.register(ObservacionMantenimiento)
+class ObservacionMantenimientoAdmin(admin.ModelAdmin):
+    list_display = ['habitacion', 'observacion', 'creado_en']
+    list_filter = ['creado_en']
+    list_select_related = ['habitacion']
+    readonly_fields = ['creado_en']
