32076bf7c4f7eb7b6ce63849be314e4e2b099d30
juancordova-b <cbarrantesjuanc@uss.edu.pe>
2026-07-17 20:21:45 -0500

    Agrega signal post_save para registrar creacion de habitaciones en log

--- habitaciones/signals.py
+++ habitaciones/signals.py
@@ -1,1 +1,20 @@
-"""Signals del módulo de habitaciones."""
+import logging
+
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from .models import Habitacion
+
+logger = logging.getLogger('hotel.operaciones')
+
+
+@receiver(post_save, sender=Habitacion)
+def log_crear_habitacion(sender, instance, created, **kwargs):
+    if created:
+        logger.info(
+            'Habitacion creada: %s en hotel %s (tipo %s, piso %d)',
+            instance.numero,
+            instance.hotel.nombre,
+            instance.tipo.nombre,
+            instance.piso,
+        )
