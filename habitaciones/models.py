eddf20f96a90f3f949fac2e1e9a17ef9548b8191
juancordova-b <cbarrantesjuanc@uss.edu.pe>
2026-07-17 20:19:33 -0500

    Agrega campo amenidades JSONField a TipoHabitacion

--- habitaciones/models.py
+++ habitaciones/models.py
@@ -5,6 +5,7 @@ class TipoHabitacion(models.Model):
     nombre = models.CharField(max_length=80)
     capacidad = models.PositiveIntegerField(default=2)
     precio_base = models.DecimalField(max_digits=10, decimal_places=2)
+    amenidades = models.JSONField(default=dict, blank=True)
 
     def __str__(self):
         return self.nombre
