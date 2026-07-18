2ee3b12ae401ffbcf61c2d8caedea5334eac0409
juancordova-b <cbarrantesjuanc@uss.edu.pe>
2026-07-17 20:19:00 -0500

    Agrega modelo Hotel como dependencia para el modulo habitaciones

--- hoteles/models.py
+++ hoteles/models.py
@@ -1,1 +1,15 @@
-"""Modelos de hoteles pendientes de implementación."""
+from django.db import models
+
+
+class Hotel(models.Model):
+    nombre = models.CharField(max_length=150)
+    ruc = models.CharField(max_length=11, unique=True)
+    direccion = models.CharField(max_length=250)
+    estrellas = models.PositiveIntegerField(default=3)
+    telefono = models.CharField(max_length=20, blank=True)
+
+    class Meta:
+        ordering = ['nombre']
+
+    def __str__(self):
+        return self.nombre
