be83f230d121539144ed0532141c3f320c40fafb
juancordova-b <cbarrantesjuanc@uss.edu.pe>
2026-07-17 20:19:25 -0500

    Crea modelo TipoHabitacion con campos nombre, capacidad y precio_base

--- habitaciones/models.py
+++ habitaciones/models.py
@@ -1,1 +1,10 @@
-"""Modelos de habitaciones pendientes de implementación."""
+from django.db import models
+
+
+class TipoHabitacion(models.Model):
+    nombre = models.CharField(max_length=80)
+    capacidad = models.PositiveIntegerField(default=2)
+    precio_base = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.nombre
